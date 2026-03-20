1969d5d38ab21080b3f2d85d44f336765907dc78
miangee21 <mianhassan5520@gmail.com>
2026-03-20 23:06:08 +0500

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
+import { Button } from "@base-ui/react/button";
 import "./App.css";
 
 function App() {
-
   return (
-   <><h1 className="text-3xl text-pink-900 font-bold underline">
-    Hello world!
-  </h1></>
+    <>
+      <h1 className="text-3xl text-pink-900 font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click Me</Button>
+    </>
   );
 }
 
